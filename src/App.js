c5eba730a9d0fd97cc29d0dd311bd2918dadfdb2
Uyen Truong <haiuyentruong@gmail.com>
2020-11-07 09:25:08 +0200

    Break search into components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 import React from 'react'
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import TextField from '@material-ui/core/TextField';
-import SearchIcon from '@material-ui/icons/Search';
-import Button from '@material-ui/core/Button';
+import Search from './components/Search'
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Container className="App" maxwidth="sm">
-        <Typography variant="h3" component="h2" gutterBottom>
-          RenoScope
-        </Typography>
-        <Typography variant="h5" gutterBottom>
-          Gaze into the crystal! Ooh, tell me the details of the next renovations at...
-        </Typography>
-        <TextField
-          id="input-with-icon-textfield"
-          type="search"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <div>
-          <Button variant="contained" color="primary">
-            Search
-          </Button>
-        </div>
-      </Container>
-    </React.Fragment>
-    
+    <Search />
   );
 }
 
