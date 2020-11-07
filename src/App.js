ad6ac1fe99cf90d2b07a705f9c850f50ab1c28d0
Uyen Truong <haiuyentruong@gmail.com>
2020-11-07 09:21:12 +0200

    Basic search view

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Container className="App" maxwidth="sm">
+        <Typography variant="h3" component="h2" gutterBottom>
+          RenoScope
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          Gaze into the crystal! Ooh, tell me the details of the next renovations at...
+        </Typography>
+        <TextField
+          id="input-with-icon-textfield"
+          type="search"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <div>
+          <Button variant="contained" color="primary">
+            Search
+          </Button>
+        </div>
+      </Container>
+    </React.Fragment>
+    
   );
 }
 
