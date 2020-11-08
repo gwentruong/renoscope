99896173dada804b5be6ff3bfe208c5334ccd110
Uyen Truong <haiuyentruong@gmail.com>
2020-11-08 07:13:12 +0200

    Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Search from './views/Search'
+import Search from './components/Search'
 import BuildingForm from './components/BuildingForm'
 import Renovations from './components/Renovations';
 import Recomendations from './components/Recomendations';
@@ -7,11 +7,10 @@ import Map from './components/Map'
 import './App.css'
 import MapInfo from './components/MapInfo';
 import References from './components/References';
-import VerifyBuilding from './views/VerifyBuilding';
 
 const App = () => {
   return (
-    <VerifyBuilding addressCoords={[24.865098, 60.243748]} />
+    <Map addressCoords={[24.865098, 60.243748]} />
   )
 }
 
